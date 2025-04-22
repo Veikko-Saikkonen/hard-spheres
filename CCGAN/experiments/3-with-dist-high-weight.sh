04d2a2199829784cb578a7f17fd7b424133348be
veikko <veikko.saikkonen@gmail.com>
2025-04-22 19:10:39 +0300

    Update 3-with-dist-high-weight.sh

--- CCGAN/experiments/3-with-dist-high-weight.sh
+++ CCGAN/experiments/3-with-dist-high-weight.sh
@@ -11,7 +11,7 @@
 #SBATCH --mem-per-cpu=8000
 #SBATCH --gres=gpu:v100:1
 
-EXPERIMENT_NAME="2-with-dist-high-weight"
+EXPERIMENT_NAME="3-with-dist-high-weight"
 echo "Entering experiment ${EXPERIMENT_NAME}..."
 
 if [ ! -d ".venv" ]; then

c64e30be1205a6a278c55b2a006d7f3f9a790c23
veikko <veikko.saikkonen@gmail.com>
2025-05-18 16:47:31 +0300

    chore: reduced lr

--- CCGAN/experiments/3-with-dist-high-weight-limit-data-fixed.sh
+++ CCGAN/experiments/3-with-dist-high-weight-limit-data-fixed.sh
@@ -50,8 +50,8 @@ python3 train.py\
         --gen_label_dim 128\
         --disc_label_dim 128\
         --weight_dist 0.2\
-        --g_lr 0.0002\
-        --coord_lr 0.0002\
-        --dist_lr 0.0002\
+        --g_lr 0.00005\
+        --coord_lr 0.00005\
+        --dist_lr 0.00005\
         --step_size 50\
         --gamma 0.90\

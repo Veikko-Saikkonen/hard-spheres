43cc598f7d85822e09e250508dfbf809c8b9f869
veikko <veikko.saikkonen@gmail.com>
2025-06-07 15:48:00 +0300

    Update 6-large-d.sh

--- CCGAN/experiments/6-large-d.sh
+++ CCGAN/experiments/6-large-d.sh
@@ -39,10 +39,10 @@ mkdir -p "${RESULTS_DIR}"
 python3 train.py\
         --training_data ../data/processed/samples\
         --gen_int 5\
-        --gsave_freq 20\
+        --gsave_freq 5\
         --n_save 20\
         --print_freq 5\
-        --msave_freq 50\
+        --msave_freq 20\
         --msave_dir "${RESULTS_DIR}/"\
         --gsave_dir "${RESULTS_DIR}/"\
         --gen_channels_1 256\
@@ -51,8 +51,8 @@ python3 train.py\
         --gen_label_dim 128\
         --disc_label_dim 128\
         --weight_dist 0.2\
-        --g_lr 0.0005\
-        --coord_lr 0.0005\
-        --dist_lr 0.0005\
+        --g_lr 0.00005\
+        --coord_lr 0.00005\
+        --dist_lr 0.00005\
         --step_size 50\
         --gamma 0.90\
